4df542935a9c82209a5f2bcf4c420a797ac59975
Jason <13209003+j4ysanchez@users.noreply.github.com>
2024-02-25 10:17:22 -0700

    Initial Commit

---
+++ src/test/java/ca/mjsanchez/orderProcessor/OrderProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mjsanchez.orderProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
